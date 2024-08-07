61bf71579b0441ca33cb5296c50085e9536bb636
LorenzoTaroni <lorzo94@live.it>
2024-08-07 02:09:28 +0200

    Added split_string function in plot module

--- svgsa/plot_module.py
+++ svgsa/plot_module.py
@@ -7,6 +7,35 @@ from matplotlib.pyplot import rc_context
 from matplotlib.lines import Line2D
 import seaborn as sns
 
+def split_string(a):
+    """
+    Splits the input string based on the presence of space or underscore, inserts a newline character in the middle of the resulting list, and joins the elements back into a single string using the original separator. 
+    Parameters:
+    a (str): The input string to be split.
+    Returns:
+    str: The modified string after splitting and joining.
+    """
+    # if " " in a:
+    #    sep = " "
+    # elif "_" in a:
+    #     sep = "_" 
+    # else:
+    #     sep = ""
+        
+    # b = a.split(sep)
+    # b[len(b)//2] = "".join([b[len(b)//2],"\n"])
+    
+
+    # return sep.join(b)
+    step = 26
+    if len(a) > step:
+        b = []
+        for i in range(len(a)//step+1):
+            b.append("".join([a[i*step:(i+1)*step],"\n"]))
+        return "".join(b)
+    else:
+        return a
+
 def save_plot(file_name, save_directory):
     path = os.getcwd()
 
@@ -42,22 +71,25 @@ def decoupler_comparison(N_GS_list, adata, net, source='source', target='target'
         # Iterate over each value of i and create a scatter plot
         for j, ax in enumerate(axs):
             sns.scatterplot(x=adata.obsm[method][N_GS_list[j]], y=adata.obsm["last_node"][N_GS_list[j]],
-                            hue=adata.obs[N_GS_list[j]], palette="viridis", ax=ax, legend=False) #, palette=f"tab{palette_value}"
-            
+                            hue=adata.obs["gs_labels"], palette="tab10", ax=ax, legend=False) #, palette=f"tab{palette_value}"
+                                     #obs[N_GS_list[j]], palette="viridis"
+                                     #obs["gs_labels"], palette="tab10"
             #g.legend(loc='upper right', bbox_to_anchor=(1.2, 1.0), ncol=1)
 
-            norm = plt.Normalize(adata.obs[N_GS_list[j]].min(), adata.obs[N_GS_list[j]].max())
-            cmap = sns.color_palette("viridis", as_cmap=True)
-            sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-            sm.set_array([])
+            # norm = plt.Normalize(adata.obs[N_GS_list[j]].min(), adata.obs[N_GS_list[j]].max())
+            # cmap = sns.color_palette("viridis", as_cmap=True)
+            # sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+            # sm.set_array([])
 
-            cax = fig.add_axes([ax.get_position().x1 + 0.001, ax.get_position().y0, ax.get_position().width / 20, ax.get_position().height / 2])
-            ax.figure.colorbar(sm, cax=cax)
+            # cax = fig.add_axes([ax.get_position().x1 + 0.001, ax.get_position().y0, ax.get_position().width / 20, ax.get_position().height / 2])
+            # ax.figure.colorbar(sm, cax=cax)
 
             ax.set_xlabel(f'{N_GS_list[j]} {method}')
             ax.set_ylabel(f'Encoder_GSEA ending node score {j+1}')
 
     save_plot("decoupler_comparison", save_directory)
+
+
 
 def plot_loss(loss, save_directory=None):
 
@@ -77,32 +109,46 @@ def plot_learning_rate_decay(lrs, save_directory=None):
     save_plot('lr_decay', save_directory)
 
 
-def umap_embedding(adata, rep, expression_list, color_label = "bulk_labels", ncols=4, vmin=-2, vmax=2, palette=None, save_directory=None):   #sc_palette
+def umap_embedding(adata, rep, expression_list, color_label = "bulk_labels", ncols=4, dotsize=10, vmin=-2, vmax=2, n_neighbors=50, min_dist=0.6, palette=None, save_directory=None):   #sc_palette
 
     # umap hyperparameters
     random_state=42
-    n_neighbors=15
-    min_dist=0.5
+    # n_neighbors=50
+    # min_dist=0.6
     
     sc.pp.neighbors(adata, use_rep = rep, n_neighbors=n_neighbors)
     sc.tl.umap(adata, min_dist=min_dist, random_state=random_state)
     with rc_context({'figure.figsize': (3, 3)}):
         if palette:
-            sc.pl.umap(adata, color=expression_list + [color_label], s=50, frameon=False, ncols=ncols, vmin=vmin, vmax=vmax, palette=palette, show=False)
+            sc.pl.umap(adata, color=expression_list + [color_label], s=dotsize, frameon=False, ncols=ncols, vmin=vmin, vmax=vmax, palette=palette, show=False)
         else:
-            sc.pl.umap(adata, color=expression_list + [color_label], s=50, frameon=False, ncols=ncols, vmin=vmin, vmax=vmax, show=False)
+            sc.pl.umap(adata, color=expression_list + [color_label], s=dotsize, frameon=False, ncols=ncols, vmin=vmin, vmax=vmax, show=False)
         save_plot(rep, save_directory)
 
 
-def umap_full_plot(adata, rep, expression_list, color_label = "bulk_labels", dot_size=50, palette=sns.color_palette("tab10"), save_directory=None, seed = None):   #sc_palette
+def umap_full_plot(adata, rep, expression_list, color_label = "bulk_labels", dot_size=50, palette=sns.color_palette("tab10"), n_neighbors=50, min_dist=0.6, save_directory=None, seed = None):   #sc_palette
     
     names = expression_list + [color_label]
+    titles = []
+    for t in names:
+        titles.append(split_string(t))
+    #titles = names
+    fontdict = {'fontsize': 8,
+#                'fontweight': 'bold',
+#                'color': rcParams['axes.titlecolor'],
+                'verticalalignment': 'baseline',
+                'horizontalalignment': 'center'
+                }
+
     n = int(np.ceil(np.sqrt(len(names)+1)))
 
     # umap hyperparameters
-    n_neighbors=50
-    min_dist=0.6
+    # n_neighbors=50
+    # min_dist=0.6
     random_state=42
+
+    # for i in range(len(expression_list)):
+    #     adata.obs[expression_list[i]] = scale(adata.obsm['last_node'][:,i])
 
     fig = plt.figure(layout='constrained', figsize=(20, 16))
 
@@ -115,6 +161,11 @@ def umap_full_plot(adata, rep, expression_list, color_label = "bulk_labels", dot
 
 
     subfigsnestL = subfigs[0].subfigures(2, 1)#, height_ratios=[1, 1.4])
+
+
+    # std_reps = ["X","X_svgsa","X_svgsa_gs","X_svgsa_uns"]
+    # rep_titles = ['UMAP of the original data','UMAP embedding of both the latent layers combined','UMAP embedding of the latent layer gs','UMAP embedding of the latent layer uns']
+
 
     if "X" in rep:
 
@@ -130,9 +181,11 @@ def umap_full_plot(adata, rep, expression_list, color_label = "bulk_labels", dot
                 if i * n + j < (len(names)-1):
                     # Use names[i * n + j] to set the color or other parameters for the plot
                     sc.pl.umap(adata, color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, vmin=adata.obs[names[i * n + j]].min(), vmax=adata.obs[names[i * n + j]].max())
-
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
+                
                 elif i * n + j == len(names)-1:
                     sc.pl.umap(adata,color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, legend_loc=None, palette=palette)
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
 
                 elif i * n + j == len(names):
                     l1=ax.legend(handles=[
@@ -166,9 +219,11 @@ def umap_full_plot(adata, rep, expression_list, color_label = "bulk_labels", dot
                 if i * n + j < (len(names)-1):
                     # Use names[i * n + j] to set the color or other parameters for the plot
                     sc.pl.umap(adata, color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, vmin=adata.obs[names[i * n + j]].min(), vmax=adata.obs[names[i * n + j]].max())
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
 
                 elif i * n + j == len(names)-1:
                     sc.pl.umap(adata,color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, legend_loc=None, palette=palette)
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
 
                 elif i * n + j == len(names):
                     l1=ax.legend(handles=[
@@ -204,9 +259,11 @@ def umap_full_plot(adata, rep, expression_list, color_label = "bulk_labels", dot
                 if i * n + j < (len(names)-1):
                     # Use names[i * n + j] to set the color or other parameters for the plot
                     sc.pl.umap(adata, color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, vmin=adata.obs[names[i * n + j]].min(), vmax=adata.obs[names[i * n + j]].max())
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
 
                 elif i * n + j == len(names)-1:
                     sc.pl.umap(adata,color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, legend_loc=None, palette=palette)
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
 
                 elif i * n + j == len(names):
                     l1=ax.legend(handles=[
@@ -239,9 +296,11 @@ def umap_full_plot(adata, rep, expression_list, color_label = "bulk_labels", dot
                 if i * n + j < (len(names)-1):
                     # Use names[i * n + j] to set the color or other parameters for the plot
                     sc.pl.umap(adata, color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, vmin=adata.obs[names[i * n + j]].min(), vmax=adata.obs[names[i * n + j]].max())
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
 
                 elif i * n + j == len(names)-1:
                     sc.pl.umap(adata,color=names[i * n + j], ax=ax, s=dot_size, frameon=False, show=False, legend_loc=None, palette=palette)
+                    _=ax.set_title(titles[i * n + j], fontdict = fontdict)
 
                 elif i * n + j == len(names):
                     l1=ax.legend(handles=[
